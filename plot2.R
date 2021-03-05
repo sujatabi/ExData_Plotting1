06033cf524dc4d85c8a92a7ffd5df482bf1c36d8
sujatabijou <sbijou@intrahealth.org>
2021-03-04 22:00:46 -0500

    R script and png for plot 2!

---
+++ plot2.R
@@ -0,0 +1,19 @@
+library(lubridate)
+power<- read.csv("./household_power_consumption.txt",header=TRUE,sep =";",na.strings="?")
+
+power$Date <- as.Date(power$Date,"%d/%m/%Y")
+
+powerfeb <- subset(power,power$Date==as.Date("2007-02-01")|power$Date==as.Date("2007-02-02"))
+
+powerfeb$datetime <- paste(powerfeb$Date, powerfeb$Time)
+powerfeb$datetime <- strptime(datetime, "%Y-%m-%d %H:%M:%S")
+
+powerfeb$weekday <- wday(powerfeb$datetime,label=TRUE)
+
+plot(powerfeb$datetime,powerfeb$Global_active_power, ylab="Global Active Power (kilowatts)", xlab="", type="l")
+
+## Copy my plot to a PNG file
+dev.copy(png, file = "./plot2.png", width = 480, height = 480)
+
+## Don't forget to close the PNG device!
+dev.off()
